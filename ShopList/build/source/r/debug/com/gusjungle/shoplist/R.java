557213d0e31da1b0ebd5b2f3af399fba52028d85
Gustavo Suarez <gustavosuarez4@gmail.com>
2013-05-19 23:33:23 -0500

    Second Commit missing elements

--- ShopList/build/source/r/debug/com/gusjungle/shoplist/R.java
+++ ShopList/build/source/r/debug/com/gusjungle/shoplist/R.java
@@ -20,6 +20,7 @@ public final class R {
     public static final class id {
         public static final int action_help=0x7f08000c;
         public static final int action_new_shopList=0x7f08000a;
+        public static final int action_new_shopListElement=0x7f08000d;
         public static final int action_settings=0x7f08000b;
         public static final int element_detail_title_id=0x7f080002;
         public static final int layout_root=0x7f080003;
@@ -37,24 +38,26 @@ public final class R {
         public static final int activity_shop_list=0x7f030001;
         public static final int activity_shop_list_element_detail=0x7f030002;
         public static final int prompt_add_new_shop_list=0x7f030003;
-        public static final int view_shop_list_element_list =0x7f030004;
-        public static final int view_shop_list_list =0x7f030005;
+        public static final int view_shop_list_element_list=0x7f030004;
+        public static final int view_shop_list_list=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int shop_list=0x7f070001;
     }
     public static final class string {
         public static final int action_help=0x7f050000;
         public static final int action_new_shopList=0x7f050001;
-        public static final int action_settings=0x7f050002;
-        public static final int add_new_element=0x7f050003;
-        public static final int add_new_element_button=0x7f050004;
-        public static final int add_new_shopList=0x7f050005;
-        public static final int app_name=0x7f050006;
-        public static final int cancel=0x7f050007;
-        public static final int element_detail_title=0x7f050008;
-        public static final int new_shopList_budget_hint=0x7f050009;
-        public static final int new_shopList_name_hint=0x7f05000a;
+        public static final int action_new_shopListElement=0x7f050002;
+        public static final int action_settings=0x7f050003;
+        public static final int add_new_element=0x7f050004;
+        public static final int add_new_element_button=0x7f050005;
+        public static final int add_new_shopList=0x7f050006;
+        public static final int app_name=0x7f050007;
+        public static final int cancel=0x7f050008;
+        public static final int element_detail_title=0x7f050009;
+        public static final int new_shopList_budget_hint=0x7f05000a;
+        public static final int new_shopList_name_hint=0x7f05000b;
     }
     public static final class style {
         /** 
